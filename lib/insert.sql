c14d8f910a157df921f690aa0b88fdacdfee4eba
Carlos E. La Torre <amautacelt@gmail.com>
2021-01-26 11:06:45 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 35, "M", "brown", "cheerful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 12, "F", "gold", "playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 28, "F", "orange", "happy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 30, "F", "yellow", "relaxed", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 47, "F", "grey", "excited", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 22, "M", "purple", "quiet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 12, "F", "green", "worried", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 19, "M", "blue", "joyful", 0);
